452dcd1a8438f92e90225eece55513dbd3626275
aznballer420 <zzisandstormzz9@gmail.com>
2020-05-13 15:45:22 -0700

    ok ok this for sure...not in the mood

--- src/main/java/JavKing/handler/MusicPlayerManager.java
+++ src/main/java/JavKing/handler/MusicPlayerManager.java
@@ -38,14 +38,14 @@ public class MusicPlayerManager {
     private final long guildId;
     private final DiscordBot bot;
     private volatile boolean inRepeatMode = false;
-    //    private volatile int currentSongLength = 0;
+    // private volatile int currentSongLength = 0;
     private volatile int currentlyPlaying = 0;
     private volatile long currentSongStartTimeInSeconds = 0;
     private volatile long pauseStart = 0;
     private long totTimeSeconds = 0;
     private volatile LinkedList<OMusic> queue;
-//    private volatile int queueLength = 0;
-//    private YTSearch ytSearch;
+    // private volatile int queueLength = 0;
+    // private YTSearch ytSearch;
 
     public MusicPlayerManager(Guild guild, DiscordBot bot) {
         this.bot = bot;
@@ -54,7 +54,7 @@ public class MusicPlayerManager {
         player = playerManager.createPlayer();
         scheduler = new TrackScheduler(player);
         queue = new LinkedList<>();
-//        ytSearch = new YTSearch();
+        // ytSearch = new YTSearch();
         AudioManager guildManager = guild.getAudioManager();
         guildManager.setSendingHandler(new AudioPlayerSendHandler(player));
         player.addListener(scheduler);
@@ -97,18 +97,20 @@ public class MusicPlayerManager {
         return guildId;
     }
 
-    public synchronized Object playSendYTSCMessage(OMusic music, @Nullable User author, @Nullable String hex, boolean bool) {
+    public synchronized Object playSendYTSCMessage(OMusic music, @Nullable User author, @Nullable String hex,
+            boolean bool) {
         if (queue.size() > 1 && bool) {
             EmbedBuilder embed = Templates.music.added_to_queue.clearEmbed()
                     .setColor(Color.decode(BotContainer.getDotenv("YOUTUBE")))
-                    .setDescription(String.format("[%s](%s)", music.title, music.uri))
-                    .setThumbnail(music.thumbnail)
+                    .setDescription(String.format("[%s](%s)", music.title, music.uri)).setThumbnail(music.thumbnail)
                     .addField("Channel", music.author, true)
                     .addField("Song Duration", TimeUtil.millisecondsToHHMMSS(music.duration), true)
-                    .addField("ET Until Playing", TimeUtil.millisecondsToHHMMSS(totTimeSeconds - queue.get(queue.size() - 1).duration), true)
+                    .addField("ET Until Playing",
+                            TimeUtil.millisecondsToHHMMSS(totTimeSeconds - queue.get(queue.size() - 1).duration), true)
                     .addField("Position In Queue", String.valueOf(queue.size() - 1), true)
                     .setColor(Color.decode(hex != null ? hex : BotContainer.getDotenv("HEX")));
-            if (author != null) embed.setAuthor("Added to Queue!", null, author.getEffectiveAvatarUrl());
+            if (author != null)
+                embed.setAuthor("Added to Queue!", null, author.getEffectiveAvatarUrl());
             return embed;
         } else {
             return Templates.music.playing_now.formatFull("**Playing** :notes: `" + music.title + "` - Now!");
@@ -161,7 +163,8 @@ public class MusicPlayerManager {
         VoiceChannel currentChannel = bot.getJDA().getGuildById(guildId).getAudioManager().getConnectedChannel();
         if (currentChannel != null) {
             List<Member> connectedUsers = currentChannel.getMembers();
-            userList.addAll(connectedUsers.stream().filter(user -> !user.getUser().isBot() && !user.getVoiceState().isDeafened())
+            userList.addAll(connectedUsers.stream()
+                    .filter(user -> !user.getUser().isBot() && !user.getVoiceState().isDeafened())
                     .collect(Collectors.toList()));
         }
         return userList;
@@ -175,7 +178,8 @@ public class MusicPlayerManager {
     }
 
     public boolean isConnectedTo(VoiceChannel channel) {
-        return channel != null && channel.equals(channel.getJDA().getGuildById(guildId).getAudioManager().getConnectedChannel());
+        return channel != null
+                && channel.equals(channel.getJDA().getGuildById(guildId).getAudioManager().getConnectedChannel());
     }
 
     public boolean isConnected() {
@@ -185,13 +189,16 @@ public class MusicPlayerManager {
 
     public boolean leave() {
         if (!queue.isEmpty()) {
-            for (OMusic music : queue) totTimeSeconds -= music.duration;
+            for (OMusic music : queue)
+                totTimeSeconds -= music.duration;
             queue.clear();
         }
-        if (isConnected()) stopMusic();
+        if (isConnected())
+            stopMusic();
 
         Guild guild = bot.getJDA().getGuildById(guildId);
-        if (guild != null) guild.getAudioManager().closeAudioConnection();
+        if (guild != null)
+            guild.getAudioManager().closeAudioConnection();
         return true;
     }
 
@@ -201,9 +208,11 @@ public class MusicPlayerManager {
 
     public boolean isInVoiceWith(Guild guild, User author) {
         VoiceChannel channel = guild.getMember(author).getVoiceState().getChannel();
-        if (channel == null) return false;
+        if (channel == null)
+            return false;
         for (Member user : channel.getMembers()) {
-            if (user.getUser().getId().equals(guild.getJDA().getSelfUser().getId())) return true;
+            if (user.getUser().getId().equals(guild.getJDA().getSelfUser().getId()))
+                return true;
         }
         return false;
     }
@@ -226,7 +235,8 @@ public class MusicPlayerManager {
         totTimeSeconds += music.duration;
     }
 
-    public synchronized void addSCToQueue(String[] args, User author, Message message, MusicPlayerManager playerManager) {
+    public synchronized void addSCToQueue(String[] args, User author, Message message,
+            MusicPlayerManager playerManager) {
         new SCSearch().resolveSCURI(args, author, message, playerManager);
     }
 
@@ -247,29 +257,53 @@ public class MusicPlayerManager {
                     for (AudioTrack track : playlist.getTracks()) {
                         OMusic music = new OMusic();
                         music.title = track.getInfo().title;
-                        music.id = track.getSourceManager().getSourceName().equals("youtube") ? track.getIdentifier() : track.getInfo().uri;
+                        music.id = track.getSourceManager().getSourceName().equals("youtube") ? track.getIdentifier()
+                                : track.getInfo().uri;
                         music.requestedBy = author.getAsTag();
                         music.uri = track.getInfo().uri;
                         music.author = track.getInfo().author;
                         music.duration = track.getDuration();
                         if (count == 0) {
                             music.thumbnail = Util.resolveThumbnail(track, message);
-                            LPUtil.updateLPURI(SCUtil.SCisURI(music.uri) ? uri : YTUtil.getPlaylistCode(uri),
-                                    uri, playlist.getName(), music.thumbnail, message.getGuild().getId());
+                            LPUtil.updateLPURI(SCUtil.SCisURI(music.uri) ? uri : YTUtil.getPlaylistCode(uri), uri,
+                                    playlist.getName(), music.thumbnail, message.getGuild().getId());
                         }
                         addToQueue(music);
                         if (count++ == 0) {
-                            Util.sendMessage(playSendYTSCMessage(queue.isEmpty() ? queue.get(0) : queue.get(queue.size() - 1), author,
-                                    (track.getSourceManager().getSourceName().equals("youtube") ? BotContainer.getDotenv("YOUTUBE") : BotContainer.getDotenv("SOUNDCLOUD")), true), message);
+                            if (BotContainer.mongoDbAdapter.loadGuild(message.getTextChannel()).announceSongs
+                                    .equalsIgnoreCase("on")) {
+                                Object toSend = playSendYTSCMessage(
+                                        queue.isEmpty() ? queue.get(0) : queue.get(queue.size() - 1), author,
+                                        (track.getSourceManager().getSourceName().equals("youtube")
+                                                ? BotContainer.getDotenv("YOUTUBE")
+                                                : BotContainer.getDotenv("SOUNDCLOUD")),
+                                        false);
+                                if (toSend instanceof EmbedBuilder) {
+                                    Util.sendMessage(toSend,
+                                            BotContainer.mongoDbAdapter.loadGuild(message.getGuild()).channelId, bot);
+                                }
+                            } else {
+                                Util.sendMessage(playSendYTSCMessage(
+                                        queue.isEmpty() ? queue.get(0) : queue.get(queue.size() - 1), author,
+                                        (track.getSourceManager().getSourceName().equals("youtube")
+                                                ? BotContainer.getDotenv("YOUTUBE")
+                                                : BotContainer.getDotenv("SOUNDCLOUD")),
+                                        true), message);
+                            }
                         }
                     }
                 } catch (Exception ignored) {
 
                 }
                 if (!isInVoiceWith(message.getGuild(), author) && !getLinkedQueue().isEmpty()) {
-                    VoiceChannel vc = Objects.requireNonNull(Objects.requireNonNull(message.getGuild().getMember(author)).getVoiceState()).getChannel();
+                    VoiceChannel vc = Objects
+                            .requireNonNull(
+                                    Objects.requireNonNull(message.getGuild().getMember(author)).getVoiceState())
+                            .getChannel();
                     if (vc == null) {
-                        Util.sendMessage(Templates.command.x_mark.formatFull("**You must be in a voice channel first!**"), message);
+                        Util.sendMessage(
+                                Templates.command.x_mark.formatFull("**You must be in a voice channel first!**"),
+                                message);
                         return;
                     }
                     try {
@@ -278,7 +312,8 @@ public class MusicPlayerManager {
                         }
                         connectTo(vc);
                     } catch (Exception e) {
-                        Util.sendMessage(Templates.command.x_mark.formatFull("**Can't connect to voice channel, please try again!**"), message);
+                        Util.sendMessage(Templates.command.x_mark
+                                .formatFull("**Can't connect to voice channel, please try again!**"), message);
                         return;
                     }
                     startPlaying();
@@ -309,8 +344,10 @@ public class MusicPlayerManager {
         playerManager.loadItemOrdered(player, Util.idOrURI(trackToAdd), new AudioLoadResultHandler() {
             @Override
             public void trackLoaded(AudioTrack track) {
-                if (BotContainer.mongoDbAdapter.loadGuild(String.valueOf(guildId)).announceSongs.equalsIgnoreCase("on")) {
-                    Util.sendMessage(playSendYTSCMessage(trackToAdd, null, null, false), BotContainer.mongoDbAdapter.loadGuild(String.valueOf(guildId)).channelId, bot);
+                if (BotContainer.mongoDbAdapter.loadGuild(String.valueOf(guildId)).announceSongs
+                        .equalsIgnoreCase("on")) {
+                    Util.sendMessage(playSendYTSCMessage(trackToAdd, null, null, false),
+                            BotContainer.mongoDbAdapter.loadGuild(String.valueOf(guildId)).channelId, bot);
                 }
                 scheduler.queue(track);
                 startPlaying();
@@ -392,7 +429,7 @@ public class MusicPlayerManager {
     public class TrackScheduler extends AudioEventAdapter {
         private final AudioPlayer player;
         private final BlockingQueue<AudioTrack> queue;
-//        private volatile String lastRequester = "";
+        // private volatile String lastRequester = "";
 
         /**
          * @param player The audio player this scheduler uses
@@ -408,8 +445,10 @@ public class MusicPlayerManager {
          * @param track The track to play or add to queue.
          */
         public void queue(AudioTrack track) {
-            // Calling startTrack with the noInterrupt set to true will start the track only if nothing is currently playing. If
-            // something is playing, it returns false and does nothing. In that case the player was already playing so this
+            // Calling startTrack with the noInterrupt set to true will start the track only
+            // if nothing is currently playing. If
+            // something is playing, it returns false and does nothing. In that case the
+            // player was already playing so this
             // track goes to the queue instead.
             if (!player.startTrack(track, true)) {
                 queue.offer(track);
@@ -418,7 +457,7 @@ public class MusicPlayerManager {
 
         @Override
         public void onTrackStuck(AudioPlayer player, AudioTrack track, long thresholdMs) {
-//            super.onTrackStuck(player, track, thresholdMs);
+            // super.onTrackStuck(player, track, thresholdMs);
             skipTrack();
         }
 
@@ -444,7 +483,8 @@ public class MusicPlayerManager {
 
         @Override
         public void onTrackEnd(AudioPlayer player, AudioTrack track, AudioTrackEndReason endReason) {
-            // Only start the next track if the end reason is suitable for it (FINISHED or LOAD_FAILED)
+            // Only start the next track if the end reason is suitable for it (FINISHED or
+            // LOAD_FAILED)
             if (endReason.mayStartNext) {
                 if (isInRepeatMode()) {
                     player.startTrack(track.makeClone(), false);
