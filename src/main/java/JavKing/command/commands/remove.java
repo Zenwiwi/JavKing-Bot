208ca18c8b47f5b8d681417a34924713a64a71e5
aznballer420 <zzisandstormzz9@gmail.com>
2020-07-19 14:23:06 -0700

    fixed remove cmd

--- src/main/java/JavKing/command/commands/remove.java
+++ src/main/java/JavKing/command/commands/remove.java
@@ -6,11 +6,13 @@ import JavKing.handler.MusicPlayerManager;
 import JavKing.main.DiscordBot;
 import JavKing.templates.ErrorTemplate;
 import JavKing.templates.Templates;
+import JavKing.util.Util;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.entities.User;
 
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 
 public class remove extends AbstractCommand {
@@ -41,23 +43,32 @@ public class remove extends AbstractCommand {
             int pos = Integer.parseInt(args[0]), end = 0, count = 0;
             try {
                 end = Integer.parseInt(args[1]);
-            } catch (Exception ignored) {}
-            if (pos > 0 && pos < playerManager.getLinkedQueue().size() && end < playerManager.getLinkedQueue().size()) {
-                if (end != 0) {
+            } catch (ArrayIndexOutOfBoundsException | NumberFormatException ignored) {
+            }
+            if (pos > 0 && pos < playerManager.getLinkedQueue().size()) {
+                String firstSong = playerManager.getLinkedQueue().get(1).title;
+                if (end != 0 && end < playerManager.getLinkedQueue().size()) {
                     pos = Math.min(pos, end);
                     end = Math.max(pos, end);
-                    for (Iterator<OMusic> iterator = playerManager.getLinkedQueue().iterator(); iterator.hasNext();) {
-                        if (count >= pos && count < end) {
-                            iterator.remove();
+                    try {
+                        for (Iterator<OMusic> iterator = playerManager.getLinkedQueue().iterator(); iterator.hasNext(); ) {
+                            if (count >= pos && count <= end) {
+                                iterator.remove();
+                            }
+                            count++;
                         }
-                        count++;
+                    } catch (IllegalStateException | ConcurrentModificationException e) {
+                        return ErrorTemplate.formatFull(bot, this.getCommand(), channel, author, inputMessage);
                     }
                 } else
                     playerManager.getLinkedQueue().remove(pos);
+                String toReturn = "Successfully removed `" + (count == 0
+                        ? firstSong + "`"
+                        : count + "` songs starting from position `" + pos + "`");
+                return Templates.command.blue_check_mark.formatFull(Util.surround(toReturn, "**"));
             } else
                 return Templates.command.x_mark.formatFull("**Song position and/or end position must be between `1` and `" + playerManager.getLinkedQueue().size() + "`**");
         } else
             return Templates.command.x_mark.formatFull("**Song position must be between `1` and `" + playerManager.getLinkedQueue().size() + "`**");
-        return ErrorTemplate.formatFull(bot, getCommand(), channel, author, inputMessage);
     }
 }
